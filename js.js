67e371cf4aac586b305c7ee8d2a2c217028711ed
Your Name <you@example.com>
2023-01-28 19:45:30 +0330

    add localstorage in the js.js file

--- js.js
+++ js.js
@@ -2,6 +2,28 @@ const todoInput=document.querySelector(".todo-input")
 const todoPrint=document.querySelector(".todoprint")
 const todosContainer=document.querySelector(".todolist-container")
 const body=document.querySelector("body")
+let localStorageArrey= [
+
+]
+
+document.addEventListener("DOMContentLoaded",function(){
+    const todolocal=JSON.parse(localStorage.getItem("todos"))
+todolocal.forEach(todo => {
+    localStorageArrey.push(todo)
+    const divv= document.createElement("div")
+    divv.innerHTML=`   
+    <h2 class="todotext">${todo.detail} </h2>
+    <div>
+        <i class="fa fa-trash" aria-hidden="true"></i>
+        <i class="fa fa-check" aria-hidden="true"></i>
+    </div>
+    `
+    divv.setAttribute("id",todo.id)
+    divv.className="realtodoo"
+    todosContainer.appendChild(divv)
+
+});
+})
 
 todoInput.addEventListener("keydown",function(event){
 
@@ -31,9 +53,9 @@ else{
 // })
 
 
-
 todoPrint.addEventListener("click",function(){
     event.preventDefault()
+    let todoid= Math.floor(Math.random()*9999)
     if(todoInput.value===""){
         alert("fuck no fucking word in the input ")
     }else{
@@ -45,10 +67,16 @@ divv.innerHTML=`
     <i class="fa fa-check" aria-hidden="true"></i>
 </div>
 `
+divv.setAttribute("id",todoid)
 divv.className="realtodoo"
 todosContainer.appendChild(divv)
+let newData={detail:todoInput.value,id:todoid}
+localStorageArrey.push(newData)
+// localStorage.setItem("todo",JSON.stringify({detail:todoInput.value,id:todoid}))
 
 todoInput.value=""
+
+localStorage.setItem("todos",JSON.stringify(localStorageArrey))
 
     }
 
@@ -56,6 +84,13 @@ todoInput.value=""
 body.addEventListener("click",function(event){
 
 if(event.target.classList[1]==="fa-trash"){
+
+    const remid=event.target.parentElement.parentElement.id
+    const ff =localStorageArrey.find(({id})=>  id==remid )
+localStorageArrey.pop(ff)
+console.log(localStorageArrey)
+localStorage.setItem( "todos",JSON.stringify(localStorageArrey))
+
 
     event.target.parentElement.parentElement.remove()
 }
